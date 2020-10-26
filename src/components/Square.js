0f0aad1259bb9face2dfabe95d8b32dc46e99be9
kschmitz123 <kathrin_schmitz93@gmx.de>
2020-10-26 16:13:42 +0100

    Lifting State Up

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,12 +1,11 @@
 import "./square.css";
 import React, { useState } from "react";
 
-export default function Square() {
-  const [value, setValue] = useState(null); //value=state, setValue=setter function
+export default function Square(props) {
   //props= index
   return (
-    <button className="square" onClick={() => setValue("X")}>
-      {value}
+    <button className="square" onClick={() => props.onClick()}>
+      {props.value}
     </button>
   );
 }
