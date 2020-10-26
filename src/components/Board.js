3e3816ebea80c5d655c0f841cbffa3dee44a887e
kschmitz123 <kathrin_schmitz93@gmx.de>
2020-10-26 17:28:43 +0100

    Declaring a winner

--- src/components/Board.js
+++ src/components/Board.js
@@ -7,6 +7,9 @@ export default function Board() {
   const [xIsNext, setXIsNext] = useState(true);
 
   function handleClick(index) {
+    if (calculateWinner(squares) || squares[index]) {
+      return;
+    }
     const newSquares = squares.slice();
     newSquares[index] = xIsNext ? "🐧" : "🐻";
     setSquares(newSquares);
@@ -16,7 +19,13 @@ export default function Board() {
     return <Square value={squares[index]} onClick={() => handleClick(index)} />;
   }
 
-  const status = "Next player: " + (xIsNext ? "🐧" : "🐻");
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner:" + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "🐧" : "🐻");
+  }
 
   return (
     <div>
@@ -38,4 +47,24 @@ export default function Board() {
       </div>
     </div>
   );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
